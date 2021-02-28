1c9a635e3a94ab2c8ee230879f987a2bd42b9296
Ilya Protsenko <iliafreshfish@gmail.com>
2021-02-28 15:26:54 +0200

    Styles were improved. Modal rules window with close/open logic was added

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Show/hide rules
+
+const rulesBlock = document.getElementById('rules-block');
+const showModalRules = document.getElementById('show-modal-rules');
+
+showModalRules.addEventListener('click', () => {
+    rulesBlock.classList.remove('hidden');
+});
+
+const closeModalRules = document.getElementById('close-modal-rules');
+
+closeModalRules.addEventListener('click', () => {
+    rulesBlock.classList.add('hidden');
+});
+
+// Choosing of figure
+
 const paperFigure = document.getElementById('paper-figure');
 const scissorsFigure = document.getElementById('scissors-figure');
 const rockFigure = document.getElementById('rock-figure');
@@ -7,4 +24,4 @@ figures.forEach(el => {
     el.addEventListener('click', () => {
         console.log(el);
     });
-})
+});
